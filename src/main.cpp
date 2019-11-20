a654b5ebea09a00496c8dbb266cacb1d12aa6bc7
Francois <francois@orioncloud.fr>
2019-11-20 15:03:23 -0600

    Minor modifs

--- src/main.cpp
+++ src/main.cpp
@@ -23,24 +23,21 @@ using namespace gio;
 int main(int argc, char *argv[]) {
   MPI_Init(&argc, &argv);
 
-  int commRank, commRanks;
-  MPI_Comm_rank(MPI_COMM_WORLD, &commRank);
-  MPI_Comm_size(MPI_COMM_WORLD, &commRanks);
+  int Rank, NRanks;
+  MPI_Comm_rank(MPI_COMM_WORLD, &Rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &NRanks);
 
   bool UseAOS = false;
   int a = 1;
-  if (argc > 1 && string(argv[a]) == "-a") {
-    UseAOS = true;
-    --argc;
-    ++a;
-  }
-
+  
   if(argc != 2) {
-    fprintf(stderr,"USAGE: %s [-a] <mpiioName>\n", argv[0]);
+    fprintf(stderr,"USAGE: %s <mpiioName>\n", argv[0]);
     exit(-1);
   }
 
   char *mpiioName = argv[a];
+  
+  GenericIO::setNaturalDefaultPartition();
 
   vector<POSVEL_T> xx, yy, zz, vx, vy, vz, phi;
   vector<ID_T> id;
@@ -57,19 +54,28 @@ int main(int argc, char *argv[]) {
     Method = GenericIO::FileIOMPI;
 
   { // scope GIO
-  GenericIO GIO(
-    MPI_COMM_WORLD,
-    mpiioName, Method);
-  GIO.openAndReadHeader(GenericIO::MismatchRedistribute);
+    GenericIO GIO(
+        MPI_COMM_WORLD,
+        mpiioName, Method);
+    GIO.openAndReadHeader(GenericIO::MismatchRedistribute);
+    
+    int NR = GIO.readNRanks();
+        if (!Rank && NR != NRanks) {
+            cout << "Redistributing data from " << NR << " ranks to " << NRanks <<
+                    " ranks; dropping rank topology information!\n";
+        }
 
-  MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
 
-  Np = GIO.readNumElems();
-
-  if (UseAOS) {
-    pos.resize(Np + (GIO.requestedExtraSpace() + sizeof(pos_t) - 1)/sizeof(pos_t));
-    vel.resize(Np + (GIO.requestedExtraSpace() + sizeof(pos_t) - 1)/sizeof(pos_t));
-  } else {
+    Np = GIO.readNumElems();
+    
+    double PhysOrigin[3], PhysScale[3];
+    GIO.readPhysOrigin(PhysOrigin);
+    GIO.readPhysScale(PhysScale);
+    
+    cout << "PhysOrigin " << PhysOrigin << " ; PhysScale " << PhysScale << endl;
+    cout << "Parts " << Np << endl;
+    
     xx.resize(Np + GIO.requestedExtraSpace()/sizeof(POSVEL_T));
     yy.resize(Np + GIO.requestedExtraSpace()/sizeof(POSVEL_T));
     zz.resize(Np + GIO.requestedExtraSpace()/sizeof(POSVEL_T));
@@ -77,14 +83,9 @@ int main(int argc, char *argv[]) {
     vy.resize(Np + GIO.requestedExtraSpace()/sizeof(POSVEL_T));
     vz.resize(Np + GIO.requestedExtraSpace()/sizeof(POSVEL_T));
     phi.resize(Np + GIO.requestedExtraSpace()/sizeof(POSVEL_T));
-  }
-  id.resize(Np + GIO.requestedExtraSpace()/sizeof(ID_T));
-  mask.resize(Np + GIO.requestedExtraSpace()/sizeof(MASK_T));
+    id.resize(Np + GIO.requestedExtraSpace()/sizeof(ID_T));
+    mask.resize(Np + GIO.requestedExtraSpace()/sizeof(MASK_T));
 
-  if (UseAOS) {
-    GIO.addVariable("pos", pos, GenericIO::VarHasExtraSpace);
-    GIO.addVariable("vel", vel, GenericIO::VarHasExtraSpace);
-  } else {
     GIO.addVariable("x", xx, GenericIO::VarHasExtraSpace);
     GIO.addVariable("y", yy, GenericIO::VarHasExtraSpace);
     GIO.addVariable("z", zz, GenericIO::VarHasExtraSpace);
@@ -92,27 +93,23 @@ int main(int argc, char *argv[]) {
     GIO.addVariable("vy", vy, GenericIO::VarHasExtraSpace);
     GIO.addVariable("vz", vz, GenericIO::VarHasExtraSpace);
     GIO.addVariable("phi", phi, GenericIO::VarHasExtraSpace);
-  }
-  GIO.addVariable("id", id, GenericIO::VarHasExtraSpace);
-  GIO.addVariable("mask", mask, GenericIO::VarHasExtraSpace);
+    GIO.addVariable("id", id, GenericIO::VarHasExtraSpace);
+    GIO.addVariable("mask", mask, GenericIO::VarHasExtraSpace);
 
-  GIO.readData();
+    GIO.readData();
   } // destroy GIO prior to calling MPI_Finalize
 
-  if (UseAOS) {
-    pos.resize(Np);
-    vel.resize(Np);
-  } else {
-    xx.resize(Np);
-    yy.resize(Np);
-    zz.resize(Np);
-    vx.resize(Np);
-    vy.resize(Np);
-    vz.resize(Np);
-    phi.resize(Np);
-  }
+  xx.resize(Np);
+  yy.resize(Np);
+  zz.resize(Np);
+  vx.resize(Np);
+  vy.resize(Np);
+  vz.resize(Np);
+  phi.resize(Np);
   id.resize(Np);
   mask.resize(Np);
+  
+  
 
   MPI_Barrier(MPI_COMM_WORLD);
   MPI_Finalize();
